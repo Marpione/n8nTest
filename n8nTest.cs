4edc3b26ad23b40b1a697fa2295b8b44569c6ae4
Marpione <ugurtanerkazan@gmail.com>
2025-02-09 00:39:48 +0300

    Update n8nTest.cs

--- n8nTest.cs
+++ n8nTest.cs
@@ -1,1 +1,19 @@
-//test
+using System;
+
+class Circle
+{
+    public double GetCircumference(double radius)
+    {
+        return 2 * 3.14159 * radius; // Magic number: 3.14159
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle();
+        double circumference = circle.GetCircumference(10); // Magic number: 10
+        Console.WriteLine("Circumference: " + circumference);
+    }
+}
